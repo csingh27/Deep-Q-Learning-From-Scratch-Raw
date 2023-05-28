8b02fa78a80111da579cc6981335a78d83ee9804
Chandandeep Singh <singhchandandeep34@gmail.com>
2023-05-28 20:37:36 +0200

    Take random actions

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 # Define environment 
 import gymnasium as gym
-env = gym.make("ALE/Breakout-v5")
+env = gym.make("ALE/Breakout-v5", render_mode = 'human')
+
+done = False
+while not done:
+    state = env.reset()
+    action = env.action_space.sample()
+    print(action) 
+    # action_space = Discrete(4) 
+    # 0 NOOP (No action)
+    # 1 FIRE
+    # 2 RIGHT
+    # 3 LEFT
+    # state, action, reward, done = 
+    env.step(action)
+    env.render()
+
+env.close()
 
 # Define agent
 
