2422d81fd74c0f74b9cac3a6be6733b06c7a7e2f
Chandandeep Singh <singhchandandeep34@gmail.com>
2023-05-29 11:37:34 +0200

    Dataset preprocessing

--- main.py
+++ main.py
@@ -13,9 +13,29 @@ import numpy as np
 # A1.5 Rendering actions
 # A1.6 Understanding observation space
 
+# Dataset pre-processing
+def frame_preprocessing(frame):
+    # A2 Dataset preprocessing
+    # A2.1 Convert RGB to gray scale
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # A2.2 Downsampling to 110x84
+    new_width = 110
+    new_height = 84
+    downsampled_frame = cv2.resize(gray_frame, (110, 84))
+    # A2.3 Cropping the image
+    state = downsampled_frame[110-84:,:]
+
+    return state
+
 done = False
 while not done:
     state = env.reset()
+    frame = np.asarray(state[0])
+    print(frame.shape)
+
+    state = frame_preprocessing(frame)
+    print(state.shape)
+
     # Random action
     action = env.action_space.sample()
     print(action) 
@@ -26,16 +46,16 @@ while not done:
     # 3 LEFT
     # state, action, reward, done = 
     env.step(action)
+
     # frame = env.render()
     print(env.observation_space)
-    frame = np.asarray(state[0])
-    print(frame.shape)
+
+    # Display frame
     cv2.imshow("Frame", frame)
+    cv2.imshow("State", state)
     cv2.waitKey(0)
 
 env.close()
-
-# Dataset pre-processing
 
 # Define agent
 # def dqn: 
