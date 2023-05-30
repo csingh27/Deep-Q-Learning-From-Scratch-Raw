06f68f80947b210ed2a84857c4ee0b0d521fcf7a
Chandandeep Singh <singhchandandeep34@gmail.com>
2023-05-30 07:44:40 +0200

    Update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import gymnasium as gym
 env = gym.make("ALE/Breakout-v5", render_mode = 'human')
 import torch as t
+import torch.nn as nn
 import cv2
 import numpy as np
 import pandas as pd
@@ -18,6 +19,7 @@ import pandas as pd
 # A3.2 Define a dataframe for experience replay buffer
 columns = ["State", "Action", "Reward", "Done"]
 rb = pd.DataFrame(columns=columns)
+num_actions = 4
 
 # A2 Dataset pre-processing
 def frame_preprocessing(frame):
@@ -49,12 +51,53 @@ def mini_batch(rb):
 def state_sequence(rb):
     seq = rb.tail(4)
     rb = rb.iloc[:-4]
-    return rb
+    return seq, rb
 
 done = False
 # while not done:
 
 replay_buffer_size = 1000
+
+# Define agent
+class DQN(nn.Module):
+    def __init__(self, input_shape, num_actions):
+        
+        super(DQN, self).__init__()
+
+        self.conv1 = nn.Conv2d(input_shape[0], 16, kernel_size=8, stride=4)
+        self.relu1 = nn.ReLU()
+
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=2, stride=2)
+        self.relu2 = nn.ReLU()
+
+        self.fc3 = nn.Linear(self.fc_input_size(input_shape), 256)
+        self.relu3 = nn.ReLU()
+
+        self.fc4 = nn.Linear(256, num_actions)
+
+    def fc_input_shape(self, input_shape):
+
+        dummy_input = torch.zeros(1, *input_shape)
+        dummy_output = self.conv2(self.relu(self.conv1(dummy_input)))
+
+        return int(dummy_output.view(-1).size()[0])
+
+    def forward(self, x):
+
+        x = self.relu1(self.conv1(x))
+        x = self.relu2(self.conv2(x))
+
+        x = x.view(x.size(0), -1)
+        x = self.relu3(self.fc3(x))
+        x = self.fc4(x)
+
+        return x
+
+    def q_value():
+        return 0
+
+    def loss_function():
+        return 0
 
 for i in range(0, replay_buffer_size):
     state = env.reset()
@@ -86,51 +129,19 @@ print(replay_buffer)
 
 rb = replay_buffer
 
+def train():
+    mini_batch = 32
+
+
 while not done:
     state = env.reset()
     seq, rb = state_sequence(rb)
     state = seq["State"]
-    action = dqn.forward(state)
+    dqn = DQN(state.shape, num_actions )
+    action = DQN.forward(state)
     env.step(action)
 
 env.close()
-
-# Define agent
-class DQN(nn.Module):
-    def __init__(self, input_shape, num_actions:
-        
-        super(DQN, self).__init__())
-
-        self.conv1 = nn.Conv2d(input_shape[0], 16, kernel_size=8, stride=4)
-        self.relu1 = nn.ReLU()
-
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=2, stride=2)
-        self.relu2 = nn.ReLU()
-
-        self.fc3 = nn.Linear(self.fc_input_size(input_shape), 256)
-        self.relu3 = nn.ReLU()
-
-        self.fc4 = nn.Linear(256, num_actions)
-
-    def fc_input_shape(self, input_shape):
-
-        dummy_input = torch.zeros(1, *input_shape)
-        dummy_output = self.conv2(self.relu(self.conv1(dummy_input)))
-
-        return int(dummy_output.view(-1).size()[0])
-
-    def forward(self, x):
-
-        x = self.relu1(self.conv1(x))
-        x = self.relu2(self.conv2(x))
-
-        x = x.view(x.size(0), -1)
-        x = self.relu3(self.fc3(x))
-        x = self.fc4(x)
-
-        return x
-
-
 
 
 # Define DQN network
