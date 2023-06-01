8f290d2c6f67f4bec423eef2f131f2ea6f88cfbb
Chandandeep Singh <singhchandandeep34@gmail.com>
2023-06-01 13:35:59 +0200

    Update jupyter notebooks

---
+++ Tutorial/breakout_dqn_rm.py
@@ -0,0 +1,39 @@
+
+
+class DQNAgent:
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    for e in range(EPISODES):
+
+        while not done:
+
+
+
+            # if done, plot the score over episodes
+            if done:
+                if global_step > agent.train_start:
+                    stats = [score, agent.avg_q_max / float(step), step,
+                             agent.avg_loss / float(step)]
+                    for i in range(len(stats)):
+                        agent.sess.run(agent.update_ops[i], feed_dict={
+                            agent.summary_placeholders[i]: float(stats[i])
+                        })
+                    summary_str = agent.sess.run(agent.summary_op)
+                    agent.summary_writer.add_summary(summary_str, e + 1)
+
+                print("episode:", e, "  score:", score, "  memory length:",
+                      len(agent.memory), "  epsilon:", agent.epsilon,
+                      "  global_step:", global_step, "  average_q:",
+                      agent.avg_q_max / float(step), "  average loss:",
+                      agent.avg_loss / float(step))
+
+                agent.avg_q_max, agent.avg_loss = 0, 0
+
+        if e % 1000 == 0:
+            agent.model.save_weights("./save_model/breakout_dqn.h5")
